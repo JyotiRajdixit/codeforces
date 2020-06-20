10b744441080fd9a491bb34031cbeebf68d03ea5
JyotiRajdixit <47452906+JyotiRajdixit@users.noreply.github.com>
2020-06-20 13:34:35 +0530

    Create 1196A.cpp

---
+++ 1196A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+ int t;
+ long long int a,b,c;
+ 
+ cin>>t;
+ while(t--){
+ 	cin>>a>>b>>c;
+ 	cout<<(a+b+c)/2<<'\n';
+ }
+ return 0;
+}
+ 
